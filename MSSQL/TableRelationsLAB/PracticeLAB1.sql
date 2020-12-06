02f05807b2d97360bf91a501c24a0006c5ec5eaa
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-12-06 11:43:11 +0200

    Add SQL files

--- MSSQL/TableRelationsLAB/PracticeLAB1.sql
+++ MSSQL/TableRelationsLAB/PracticeLAB1.sql
@@ -26,5 +26,38 @@ ALTER TABLE Article
 ALTER TABLE Comment
     ADD ArticleId INT FOREIGN KEY REFERENCES Article (Id)
 
+ALTER TABLE Comment
+    ADD AuthorId int FOREIGN KEY REFERENCES Authors (Id)
 
+CREATE TABLE ArticleAuthors
+(
+    AuthorId  int FOREIGN KEY REFERENCES Authors (Id),
+    ArticleId int FOREIGN KEY REFERENCES Article (Id)
+)
+USE Geography
+
+SELECT *
+FROM CountriesRivers
+         JOIN Countries C ON CountriesRivers.CountryCode = C.CountryCode
+
+SELECT CountryName, ContinentName
+FROM Countries
+         JOIN Continents C ON Countries.ContinentCode = C.ContinentCode
+
+USE Blog
+
+SELECT Ar.Title, c.Text, A.FirstName, A.LastName
+FROM Comment C
+         JOIN Authors A
+              ON C.Id = A.Id
+         JOIN Article Ar
+              ON C.Id = Ar.Id
+
+USE Geography
+
+SELECT MountainRange, PeakName, Elevation
+FROM Peaks
+         JOIN Mountains M ON M.Id = Peaks.MountainId
+WHERE MountainRange = 'Rila'
+ORDER BY Elevation DESC
 
