fb6fe6320ee183b6cca66742dfc4a1161b976bbc
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-12-13 18:54:00 +0200

    Add SQL files

---
+++ MSSQL/JoinsSubqueriesCTEsAndIndicesLAB/PracticeLAB.sql
@@ -0,0 +1,85 @@
+USE SoftUni
+
+SELECT A.AddressText, T.Name
+FROM Addresses AS A
+         JOIN Towns T ON A.TownID = T.TownID
+
+SELECT *
+FROM Addresses;
+
+SELECT *
+FROM Addresses AS a
+         LEFT JOIN Towns T ON a.TownID = T.TownID;
+/*even when id is null take the left table and join the other*/
+
+SELECT *
+FROM Addresses AS a
+         RIGHT JOIN Towns T ON T.TownID = a.TownID;
+
+SELECT *
+FROM Addresses AS a
+         FULL OUTER JOIN Towns T ON T.TownID = a.TownID/*left+right*/
+
+SELECT *
+FROM Addresses
+         CROSS JOIN Towns T/*Cartesian product*/
+
+SELECT *
+FROM Employees AS e
+         JOIN Addresses A ON e.AddressID = A.AddressID
+         JOIN Towns T ON T.TownID = A.TownID;
+
+SELECT TOP (50) e.FirstName, e.LastName, T.Name, AddressText
+FROM Employees AS e
+         JOIN Addresses A ON e.AddressID = A.AddressID
+         JOIN Towns T ON T.TownID = A.TownID
+ORDER BY FirstName, LastName
+
+SELECT e.EmployeeID, FirstName, LastName, d.Name
+FROM Employees AS e
+         JOIN Departments d ON d.DepartmentID = e.DepartmentID
+WHERE Name LIKE 'Sales'
+ORDER BY EmployeeID
+
+SELECT FirstName, LastName, HireDate, Name AS 'DeptName'
+FROM Employees AS e
+         JOIN Departments d ON e.DepartmentID = d.DepartmentID
+WHERE HireDate > '1999-01-01'
+  AND Name IN ('Sales', 'Finance')
+ORDER BY HireDate
+
+SELECT TOP (50) e.EmployeeID,
+                CONCAT(e.FirstName, ' ', e.LastName)
+                       AS 'EmployeeName',
+                CONCAT(m.FirstName, ' ', m.LastName)
+                       AS 'ManagerName',
+                d.Name AS DepartmentName
+FROM Employees AS e
+         LEFT JOIN Employees AS m ON e.ManagerID = m.EmployeeID
+         LEFT JOIN Departments D ON e.EmployeeID = D.ManagerID
+ORDER BY e.EmployeeID
+
+SELECT *
+FROM Employees AS e
+WHERE e.DepartmentID IN
+      (
+          SELECT DepartmentID
+          FROM Departments
+          WHERE Name = 'Finance'
+      )
+
+SELECT top(1)
+       (SELECT AVG(Salary)
+        FROM Employees e
+        WHERE e.DepartmentID = d.DepartmentID
+       ) as MinAverageSalary
+FROM Departments d
+WHERE (SELECT count(*)
+FROM Employees e WHERE e.DepartmentID=d.DepartmentID) > 0
+ORDER BY  MinAverageSalary
+
+
+
+
+
+
