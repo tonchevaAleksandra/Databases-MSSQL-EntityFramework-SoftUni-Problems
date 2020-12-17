d1f563ae397b9deb20dbdaa66dc5016873e2108a
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-12-17 19:28:28 +0200

    Add SQL files

---
+++ MSSQL/FunctionsAndStoredProceduresLAB/CreateFunction.sql
@@ -0,0 +1,69 @@
+USE SoftUni
+
+CREATE FUNCTION udf_ProjectDurationWeeks(@StartDate DATETIME,
+                                         @EndDate DATETIME)
+    RETURNS int
+BEGIN
+    DECLARE @projectWeeks int;
+    IF (@EndDate IS NULL)
+        BEGIN
+            SET @EndDate = GETDATE()
+        END
+    SET @projectWeeks = DATEDIFF(WEEK, @StartDate, @EndDate)
+    RETURN @projectWeeks
+END
+
+
+CREATE OR
+ALTER FUNCTION udf_Pow(@Base int, @Exp int)
+    RETURNS decimal
+AS
+BEGIN
+    DECLARE @result decimal(38, 0)=1;
+    WHILE (@Exp > 0)
+        BEGIN
+            SET @result = @result * @Base;
+            SET @Exp -= 1;
+        END
+    RETURN @result
+END
+
+SELECT dbo.udf_Pow(2, 50), POWER(2, 10)
+
+SELECT *, dbo.udf_ProjectDurationWeeks(StartDate, EndDate) AS WeekDiff
+FROM Projects
+
+CREATE OR
+ALTER FUNCTION udf_GetEmployeesCountByYear(@year int)
+    RETURNS TABLE
+        AS
+        RETURN
+            (
+                SELECT *
+                FROM Employees
+                WHERE DATEPART(YEAR, HireDate) = @year
+            )
+
+SELECT *
+FROM udf_GetEmployeesCountByYear(2001)
+
+
+CREATE OR
+ALTER FUNCTION udf_Squares(@count int)
+    RETURNS @squares TABLE
+                     (
+                         Id     int PRIMARY KEY IDENTITY,
+                         Square bigint
+                     )
+AS
+BEGIN
+    DECLARE @i int=1;
+    WHILE (@i <= @count)
+        BEGIN
+            INSERT INTO @squares(Square) VALUES (@i * @i)
+            SET @i+=1;
+        END
+    RETURN
+END
+
+
---
+++ MSSQL/FunctionsAndStoredProceduresLAB/PracticeLAB.sql
@@ -0,0 +1,69 @@
+DECLARE @Year smallint=2020;
+DECLARE @Year2 int=2021;
+
+SET @Year = 2022;
+
+IF (@Year = DATEPART(YEAR, GETDATE()))
+    BEGIN
+        SELECT GETDATE()
+        SET @Year = YEAR(GETDATE()) + 1
+    END
+ELSE
+    IF (@Year = 2029)
+        SELECT '2029!'
+    ELSE
+        SELECT 'No Match!'
+
+
+DECLARE @Year2 smallint=2022
+SELECT CASE @Year2
+           WHEN 2020 THEN '2020'
+           WHEN 2021 THEN '2021'
+           ELSE
+               'Invalid year!'
+           END
+
+
+USE SoftUni
+
+DECLARE @Year int= 1999;
+
+WHILE @Year <= 2005
+    BEGIN
+        IF (@Year = 2002)
+            CONTINUE
+        SELECT @Year, COUNT(*)
+        FROM Employees
+        WHERE DATEPART(YEAR, HireDate) = @Year;
+        SET @Year = @Year + 1;
+
+        IF (@Year = 2003)
+            BREAK
+    END
+
+SELECT @Year;
+
+
+CREATE VIEW v_GeetSomeConstants AS
+SELECT PI()         AS PI,
+       GETDATE()    AS Now,
+       RAND()       AS Rand,
+       SQRT(10)        Sqrt10,
+       POWER(2, 10) AS TwoPowerOf10;
+
+SELECT *
+FROM v_GeetSomeConstants;
+
+
+/*scalar function- returns only one value*/
+/*table- value functions return table - it's like view with parameters
+  -inline TVF
+  -multi-statement MST
+ FUNCTIONS CAN NOT CHANGE THE DATABASE (INSERT, DROP, IPDATE ETC */
+
+DECLARE @TableName nvarchar(9)='Employees';
+
+EXEC ('select * from' +@TableName)
+
+
+
