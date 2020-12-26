30c8bba0f91a8c7ff9d6a625dbe6e892126478bf
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-12-26 16:25:35 +0200

    Add SQL files

--- MSSQL/EXAMS/MS SQL Exam – 16 Apr 2019/Airport.sql
+++ MSSQL/EXAMS/MS SQL Exam – 16 Apr 2019/Airport.sql
@@ -173,9 +173,10 @@ FROM Passengers AS p
          LEFT JOIN Tickets T ON p.Id = T.PassengerId
          LEFT JOIN Flights F ON F.Id = T.FlightId
          LEFT JOIN Planes P2 ON P2.Id = F.PlaneId
-         LEFT JOIN Luggages L ON p.Id = L.PassengerId
+         LEFT JOIN Luggages L ON T.LuggageId = L.Id
          LEFT JOIN LuggageTypes LT ON L.LuggageTypeId = LT.Id
-WHERE T.PassengerId is not null and T.FlightId is NOT  null
+WHERE T.PassengerId IS NOT NULL
+  AND T.FlightId IS NOT NULL
 ORDER BY [Full Name],
          [Plane Name],
          F.Origin,
@@ -184,5 +185,132 @@ ORDER BY [Full Name],
 
 GO
 
+SELECT RankingQuery.FirstName   AS [First Name],
+       RankingQuery.LastName    AS [Last Name],
+       RankingQuery.Destination AS Destination,
+       RankingQuery.Price
+FROM (
+         SELECT P.FirstName,
+                P.LastName,
+                F.Destination,
+                Price,
+                DENSE_RANK() OVER
+                    (PARTITION BY P.FirstName,P.LastName ORDER BY T.Price DESC) AS Rank
+         FROM Tickets AS T
+                  JOIN Passengers P ON P.Id = T.PassengerId
+                  JOIN Flights F ON T.FlightId = F.Id) AS RankingQuery
+WHERE RankingQuery.Rank = 1
+ORDER BY RankingQuery.Price DESC, RankingQuery.FirstName, RankingQuery.LastName, RankingQuery.Destination
+GO
 
+
+SELECT Destination, COUNT(*) AS FilesCount
+FROM Flights
+         JOIN Tickets T ON Flights.Id = T.FlightId
+GROUP BY Destination
+ORDER BY FilesCount DESC, Destination
+GO
+
+
+SELECT p.Name, p.Seats, COUNT(T.id) AS [Passengers Count]
+FROM Planes AS p
+         JOIN Flights F ON p.Id = F.PlaneId
+         JOIN Tickets T ON F.Id = T.FlightId
+GROUP BY p.Name, p.Seats
+ORDER BY [Passengers Count] DESC, p.Name, p.Seats
+GO
+
+
+
+CREATE OR
+ALTER FUNCTION udf_CalculateTickets(@origin varchar(50),
+                                    @destination varchar(50),
+                                    @peopleCount int)
+    RETURNS varchar(200) AS
+BEGIN
+    IF (@peopleCount <= 0)
+        BEGIN
+            RETURN 'Invalid people count!';
+        END
+
+    IF (SELECT COUNT(*)
+        FROM Flights
+        WHERE Origin = @origin
+          AND Destination = @destination) < 1
+        BEGIN
+
+            RETURN 'Invalid flight!'
+
+        END
+
+    DECLARE @TotalPrice decimal(18, 2)=(SELECT SUM(price)
+                                        FROM Tickets
+                                                 JOIN Flights F ON F.Id = Tickets.FlightId
+                                        WHERE Origin = @origin
+                                          AND Destination = @destination) * @peopleCount
+    RETURN 'Total price ' + CAST(@TotalPrice AS varchar(30))
+END
+
+SELECT dbo.udf_CalculateTickets('Kolyshley', 'Rancabolang', 33)
+
+GO
+
+SELECT dbo.udf_CalculateTickets('Kolyshley', 'Rancabolang', -1)
+SELECT dbo.udf_CalculateTickets('Invalid', 'Rancabolang', 33)
+
+GO
+
+
+CREATE PROC usp_CancelFlights
+AS
+BEGIN
+
+
+    UPDATE Flights
+    SET ArrivalTime= NULL,
+        DepartureTime=NULL
+    WHERE ArrivalTime >= Flights.DepartureTime
+
+END
+GO
+
+EXEC usp_CancelFlights
+/*
+SELECT *
+from Flights
+WHERE ArrivalTime is NULL and DepartureTime is NULL
+GO
+*/
+
+CREATE TABLE DeletedPlanes
+(
+    Id    int PRIMARY KEY IDENTITY,
+    Name  varchar(30) NOT NULL,
+    Seats int         NOT NULL,
+    Range int         NOT NULL
+)
+
+CREATE TRIGGER tr_InsertPlaneAfterDelete
+    ON Planes
+    AFTER DELETE AS
+BEGIN
+    INSERT INTO DeletedPlanes
+    SELECT d.Name, d.Seats, d.Range
+    FROM deleted AS d
+END
+GO
+
+DELETE Tickets
+WHERE FlightId IN (SELECT Id FROM Flights WHERE PlaneId = 8)
+
+DELETE
+FROM Flights
+WHERE PlaneId = 8
+
+DELETE
+FROM Planes
+WHERE Id = 8
+
+SELECT *
+FROM DeletedPlanes
 
