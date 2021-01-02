e0e12303fcbdd526dd1626d96bcd91b25d7a7874
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-02 19:57:43 +0200

    Add SQl files to Exams

--- MSSQL/EXAMS/Database Basics MS SQL Exam – 13 October 2019/Section 1  DDL .sql
+++ MSSQL/EXAMS/Database Basics MS SQL Exam – 13 October 2019/Section 1  DDL .sql
@@ -86,3 +86,69 @@ WHERE RepositoryId = (SELECT id FROM Repositories WHERE Name LIKE 'Softuni-Teamw
 DELETE Issues
 WHERE RepositoryId = (SELECT id FROM Repositories WHERE Name LIKE 'Softuni-Teamwork')
 
+
+USE master
+
+--5.	Commits
+SELECT *
+FROM Commits
+ORDER BY Id, Message, RepositoryId, ContributorId
+
+--6.	Heavy HTML
+
+SELECT id, Name, Size
+FROM Files
+WHERE Size > 1000
+  AND Name LIKE '%html'
+ORDER BY Size DESC, id, Name
+
+--7.	Issues and Users
+
+SELECT i.id, CONCAT(u.Username, ' : ', i.Title) AS IssueAssignee
+FROM Issues AS i
+         JOIN Bitbucket.dbo.Users AS u ON i.AssigneeId = u.Id
+ORDER BY i.id DESC, IssueAssignee;
+
+--8.	Non-Directory Files
+
+SELECT f1.id, f1.Name, CONCAT(f1.size, 'KB') AS Size
+FROM Files AS f1
+         LEFT JOIN Files AS f2 ON f1.id = f2.ParentId
+WHERE f2.ParentId IS NULL
+ORDER BY id, Name, Size DESC
+
+--9.	Most Contributed Repositories
+
+SELECT TOP (5) r.Id, r.Name, COUNT(c.Id) AS Commits
+FROM Repositories AS r
+         JOIN Commits C ON r.Id = C.RepositoryId
+         JOIN RepositoriesContributors RC ON r.Id = RC.RepositoryId
+GROUP BY r.Id, r.Name
+ORDER BY Commits DESC, r.Id, r.Name;
+
+--10.	User and Files
+
+SELECT u.Username, AVG(f.size) AS Size
+FROM Bitbucket.dbo.Users AS u
+         JOIN Commits AS c ON u.Id = c.ContributorId
+         JOIN Files AS f ON c.Id = f.CommitId
+GROUP BY u.Username
+ORDER BY Size DESC, u.Username;
+
+--Section 4. Programmability
+--11.   User Total Commits
+
+CREATE FUNCTION udf_UserTotalCommits(@username varchar(30))
+    RETURNS int AS
+BEGIN
+    RETURN (SELECT COUNT(id)
+            FROM Commits
+            WHERE ContributorId = (SELECT id FROM Bitbucket.dbo.Users WHERE Username LIKE @username))
+END
+
+SELECT udf_UserTotalCommits('UnderSinduxrein')
+
+
+
+
+
