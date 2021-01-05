02a709d0388fb30fe7516c21c1fd27d3a3e1d801
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-05 20:25:43 +0200

    Add files EF

---
+++ Entity Framework/INTRO/AdoNetDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace AdoNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string connectionString = "Server=.;Database=SoftUni;User Id=Aleksandra;Password=1234";
+            string connectionString = "Server=.;Database=SoftUni;Integrated Security=true";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                string command = "SELECT COUNT(*) FROM [Employees]";
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+              object result=  sqlCommand.ExecuteScalar();
+                Console.WriteLine(result);
+
+            }
+                
+        }
+    }
+}
