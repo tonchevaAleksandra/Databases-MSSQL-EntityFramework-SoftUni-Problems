adbee563b280116702038ba8b780e5149ff38047
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-09 16:34:19 +0200

    Add files to EF

---
+++ Entity Framework/INTRO/6. Remove Villain/Queries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6._Remove_Villain
+{
+   public static class Queries
+    {
+        public const string FindVillainName = "SELECT Name FROM Villains WHERE Id = @villainId";
+
+        public const string DeleteVillainFromMV= "DELETE FROM MinionsVillains WHERE VillainId = @villainId";
+
+        public const string DeleteVillainFromVillains = "DELETE FROM Villains WHERE Id = @villainId";
+    }
+}
--- Entity Framework/INTRO/6. Remove Villain/StartUp.cs
+++ Entity Framework/INTRO/6. Remove Villain/StartUp.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 
 namespace _6._Remove_Villain
 {
-   public class StartUp
+    public class StartUp
     {
+        private static string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int villainId = int.Parse(Console.ReadLine());
+            connection.Open();
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(Queries.FindVillainName, connection);
+                command.Parameters.AddWithValue("@villainId", villainId);
+
+                using (command)
+                {
+                    object result = command.ExecuteScalar();
+
+                    if (result == null)
+                    {
+                        Console.WriteLine("No such villain was found.");
+                    }
+
+                    else
+                    {
+                        string villainName = (string)result;
+                        int affectedRows = DeleteVillainFromMV(villainId);
+                        DeleteVillainByID(villainId);
+
+                        Console.WriteLine($"{villainName} was deleted.");
+                        Console.WriteLine($"{affectedRows} minions were released.");
+
+                    }
+                }
+            }
+
+        }
+
+        private static void DeleteVillainByID(int villainId)
+        {
+            SqlCommand sqlCommand = new SqlCommand(Queries.DeleteVillainFromVillains, connection);
+            sqlCommand.Parameters.AddWithValue("@villainId", villainId);
+
+            using (sqlCommand)
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        private static int DeleteVillainFromMV(int villainId)
+        {
+            SqlCommand command = new SqlCommand(Queries.DeleteVillainFromMV, connection);
+            command.Parameters.AddWithValue("@villainId", villainId);
+
+            using (command)
+            {
+                int affectedRows = command.ExecuteNonQuery();
+                return affectedRows;
+            }
         }
     }
 }
