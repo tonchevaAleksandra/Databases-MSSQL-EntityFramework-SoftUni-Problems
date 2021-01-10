d4eceae040d4062a506cb0d26ef388b1d45ee1ae
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-10 15:18:32 +0200

    Add files to EF

--- Entity Framework/ORMFUNDAMENTALS/MiniORM/ChangeTracker.cs
+++ Entity Framework/ORMFUNDAMENTALS/MiniORM/ChangeTracker.cs
@@ -1,4 +1,122 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace MiniORM
 {
-	// TODO: Create your ChangeTracker class here.
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntitiies;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntitiies = CloneEntities(entities);
+        }
+
+        public IReadOnlyCollection<T> AllEntities =>
+            this.allEntitiies.AsReadOnly();
+
+        public IReadOnlyCollection<T> Added =>
+            this.added.AsReadOnly();
+
+        public IReadOnlyCollection<T> Removed =>
+            this.removed.AsReadOnly();
+
+        public void Add(T item) =>
+            this.added.Add(item);
+
+        public void Remove(T item) =>
+            this.removed.Add(item);
+
+        private List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            List<T> cloneEntities = new List<T>();
+
+            PropertyInfo[] propertiesToClone = typeof(T)
+                .GetProperties()
+                .Where(pi => DbContext
+                .AllowedSqlTypes
+                .Contains(pi.PropertyType))
+                .ToArray();
+
+            foreach (T entity in entities)
+            {
+                T clone = Activator.CreateInstance<T>();
+
+                foreach (PropertyInfo property in propertiesToClone)
+                {
+                    object value = property.GetValue(entity);
+                    property.SetValue(clone, value);
+                }
+
+                cloneEntities.Add(clone);
+            }
+
+            return cloneEntities;
+        }
+
+        public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+        {
+            List<T> modifiedEntities = new List<T>();
+
+           PropertyInfo[] primaryKeys = typeof(T)
+                .GetProperties()
+                .Where(pi => pi.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            foreach (T proxyEntity in this.allEntitiies)
+            {
+                object[] primaryKeyValues = GetPrimaryKeyValues(primaryKeys, proxyEntity)
+                    .ToArray();
+
+                T entity = dbSet
+                    .Entities
+                    .Single(e => GetPrimaryKeyValues(primaryKeys, e)
+                    .SequenceEqual(primaryKeyValues));
+
+                bool isModified = IsModified(proxyEntity, entity);
+
+                if (isModified)
+                {
+                    modifiedEntities.Add(entity);
+                }
+            }
+
+            return modifiedEntities;
+        }
+
+        private static bool IsModified(T proxyEntity, T entity)
+        {
+            PropertyInfo[] monitoredProperties = typeof(T)
+                 .GetProperties()
+                 .Where(pi => DbContext
+                 .AllowedSqltypes
+                 .Contains(pi.PropertyType))
+                 .ToArray();
+
+            PropertyInfo[] modifiedProperties = monitoredProperties
+                .Where(pi => !Equals(pi.GetValue(entity), pi.GetValue(proxyEntity)))
+                .ToArray();
+
+            bool isModified = modifiedProperties.Any();
+
+            return isModified;
+        }
+
+        private static IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T proxyEntity)
+        {
+            return primaryKeys
+                .Select(pk => pk.GetValue(proxyEntity));
+        }
+    }
 }
--- Entity Framework/ORMFUNDAMENTALS/MiniORM/DbSet.cs
+++ Entity Framework/ORMFUNDAMENTALS/MiniORM/DbSet.cs
@@ -1,4 +1,87 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
 {
-	// TODO: Create your DbSet class here.
+    public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
+    {
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+
+        internal IList<TEntity> Entities { get; set; }
+
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            this.Entities = entities.ToList();
+
+            this.ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+        public int Count => this.Entities.Count;
+
+        public bool IsReadOnly => this.Entities.IsReadOnly;
+
+        public void Add(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null!");
+            }
+
+            this.Entities.Add(item);
+
+            this.ChangeTracker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (this.Entities.Any())
+            {
+                TEntity entity = this.Entities.First();
+                this.Remove(entity);
+            }
+        }
+
+        public bool Contains(TEntity item)
+        => this.Entities.Contains(item);
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+       => this.Entities.CopyTo(array, arrayIndex);
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return this.Entities.GetEnumerator();
+        }
+
+        public bool Remove(TEntity item)
+        {
+            if (item==null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null!");
+            }
+
+            bool removeSuccessfully = this.Entities.Remove(item);
+
+            if (removeSuccessfully)
+            {
+                this.ChangeTracker.Remove(item);
+            }
+
+            return removeSuccessfully;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (TEntity entity in entities)
+            {
+                this.Remove(entity);
+            }
+        }
+    }
 }
