dc5945e7c4e2255d75bbaee4006e44a9f2428e5d
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-13 12:48:33 +0200

    Add files to EF

--- Entity Framework/EFCore Intro Exercises/SoftUni/StartUp.cs
+++ Entity Framework/EFCore Intro Exercises/SoftUni/StartUp.cs
@@ -6,12 +6,36 @@ using System.Text;
 
 namespace SoftUni
 {
-   public class StartUp
+    public class StartUp
     {
         static void Main(string[] args)
         {
             var context = new SoftUniContext();
-            Console.WriteLine(GetEmployeesFullInformation(context));
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e=>e.Salary>50000)
+                .OrderBy(e => e.FirstName)
+                .Select(e => new Employee
+                {
+                    FirstName = e.FirstName,
+                   
+                    Salary = e.Salary
+                })
+                .ToList();
+
+            foreach (var item in employees)
+            {
+                sb.AppendLine(item.FirstName + $" - {item.Salary:f2}");
+            }
+
+            return sb.ToString();
+
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -20,13 +44,13 @@ namespace SoftUni
 
             var employees = context.Employees
                 .OrderBy(e => e.EmployeeId)
-                .Select(e =>new Employee
+                .Select(e => new Employee
                 {
-                    FirstName=e.FirstName,
-                    LastName=e.LastName,
-                    MiddleName=e.MiddleName,
-                    JobTitle=e.JobTitle,
-                    Salary=e.Salary
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    MiddleName = e.MiddleName,
+                    JobTitle = e.JobTitle,
+                    Salary = e.Salary
                 })
                 .ToList();
 
@@ -36,6 +60,6 @@ namespace SoftUni
             }
 
             return sb.ToString();
-        }         
+        }
     }
 }
