5f869a0cc735a1be8f8e55b89546a31f9f2ebad2
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-17 20:40:26 +0200

    Add files to EF Core

---
+++ Entity Framework/Entity Relations/P03_FootballBetting.Web/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P03_FootballBetting.Data;
+using P03_FootballBetting.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P03_FootballBetting.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly FootballBettingContext footballBettingContext;
+
+        public UsersController(FootballBettingContext footballBettingContext)
+        {
+            this.footballBettingContext = footballBettingContext;
+        }
+        public IActionResult Index()
+        {
+            return this.RedirectToRoute("/Users/All");
+        }
+        [HttpGet]
+        public IActionResult All()
+        {
+            List<User> users = footballBettingContext.Users.ToList();
+
+            return this.View(users);
+        }
+    }
+}
