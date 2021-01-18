e6ef907218733e65153bb0bd00da710cacc08625
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-18 13:18:26 +0200

    Add files to EF Core

--- Entity Framework/Entity Relations/P03_FootballBetting.Web/Controllers/UsersController.cs
+++ Entity Framework/Entity Relations/P03_FootballBetting.Web/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using P03_FootballBetting.Data;
 using P03_FootballBetting.Data.Models;
+using P03_FootballBetting.Web.ViewModels.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace P03_FootballBetting.Web.Controllers
         }
         public IActionResult Index()
         {
-            return this.RedirectToRoute("/Users/All");
+            return this.RedirectToAction("All");
         }
         [HttpGet]
         public IActionResult All()
@@ -26,6 +27,35 @@ namespace P03_FootballBetting.Web.Controllers
             List<User> users = footballBettingContext.Users.ToList();
 
             return this.View(users);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateUserViewModel model )
+        {
+            if(!ModelState.IsValid)
+            {
+                return this.View("Error");
+            }
+           
+                User user = new User()
+                {
+                    Username = model.Username,
+                    Password = model.Password,
+                    Email = model.Email,
+                    Name = model.Name,
+                    Balance = 0M
+                };
+
+            this.footballBettingContext.Users.Add(user);
+            this.footballBettingContext.SaveChanges();
+
+            return this.RedirectToAction("All");
+           
         }
     }
 }
---
+++ Entity Framework/Entity Relations/P03_FootballBetting.Web/ViewModels/Users/CreateUserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P03_FootballBetting.Web.ViewModels.Users
+{
+    public class CreateUserViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [MinLength(4)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(26)]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
