e3b6b59b615d9e822203b9de43e24a76f4230d54
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-20 18:53:34 +0200

    Add files to EF Core

--- Entity Framework/EF Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework/EF Advanced Querying/BookShop/StartUp.cs
@@ -1,10 +1,12 @@
 ﻿namespace BookShop
 {
     using BookShop.Models;
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
     using Microsoft.EntityFrameworkCore;
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
@@ -15,12 +17,24 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            string input = Console.ReadLine();
-            string output = GetBooksByAgeRestriction(db, input);
-            Console.WriteLine(output);
+            //Proble 02
+            //string input = Console.ReadLine();
+            //string output = GetBooksByAgeRestriction(db, input);
+            //Console.WriteLine(output);
 
+            Console.WriteLine(GetGoldenBooks(db));
         }
+        //Problem 03
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            List<string> books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
 
+            return String.Join(Environment.NewLine, books);
+        }
         //Problem 02
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
