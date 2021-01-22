98486b3a9aeb820d8db1ad768be3a7ccaf21e1d8
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-22 12:37:28 +0200

    Add files to EF Core

---
+++ Entity Framework/CSharp Auto Mapping Objects/BookShop.Data/ViewModels/BookDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Data.ViewModels
+{
+   public class BookDTO
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+        // public string Author { get; set; }
+    }
+}
---
+++ Entity Framework/CSharp Auto Mapping Objects/BookShop.Models/Author.cs
@@ -0,0 +1,22 @@
+﻿namespace BookShop.Models
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    public class Author
+    {
+        public Author()
+        {
+            //this.Books = new HashSet<Book>();
+        }
+
+        public int AuthorId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Book> Books { get; set; } = new HashSet<Book>();
+    }
+}
