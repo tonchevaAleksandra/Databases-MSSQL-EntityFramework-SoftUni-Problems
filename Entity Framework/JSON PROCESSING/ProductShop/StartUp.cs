ec83583e68f82de1d6caf1b0056f076017405ded
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-25 18:46:56 +0200

    Add files to EFC JSON PROCESSING

---
+++ Entity Framework/JSON PROCESSING/ProductShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+
+        public static void Main(string[] args)
+        {
+            ProductShopContext db = new ProductShopContext();
+            //ResetDatabase(db);
+
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+
+            var result = ImportUsers(db, inputJson);
+            Console.WriteLine(result);
+        }
+
+        //Problem 1
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+
+            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            context.Users.AddRange(users);
+            int count = users.Length;
+            context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+
+        private static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Database was successfully deleted!");
+
+            db.Database.EnsureCreated();
+            Console.WriteLine("Database was successfully created!");
+        }
+    }
+}
