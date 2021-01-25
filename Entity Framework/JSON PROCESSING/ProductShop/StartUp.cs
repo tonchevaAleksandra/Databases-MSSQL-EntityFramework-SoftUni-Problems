4a7151e25f3509cc927d50b31f8fb777b5c99fb7
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-25 20:03:26 +0200

    Add files to EFC Json Processing

--- Entity Framework/JSON PROCESSING/ProductShop/StartUp.cs
+++ Entity Framework/JSON PROCESSING/ProductShop/StartUp.cs
@@ -17,19 +17,57 @@ namespace ProductShop
             ProductShopContext db = new ProductShopContext();
             //ResetDatabase(db);
 
-            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            //Problem 01
+            //string inputJson = File.ReadAllText("../../../Datasets/users.json");
 
-            var result = ImportUsers(db, inputJson);
+            //var result = ImportUsers(db, inputJson);
+            //Console.WriteLine(result);
+
+            //Problem 02
+
+            //string inputJson = File.ReadAllText("../../../Datasets/products.json");
+            //var result = ImportProducts(db, inputJson);
+            //Console.WriteLine(result);
+
+            //Problem 03
+            string inputJson = File.ReadAllText("../../../Datasets/categories.json");
+            string result = ImportCategories(db, inputJson);
             Console.WriteLine(result);
         }
 
-        //Problem 1
+        //Problem 03
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(inputJson, new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore
+            });
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
+        }
+
+        //Problem 02
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+
+        //Problem 01
         public static string ImportUsers(ProductShopContext context, string inputJson)
         {
 
-            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
             context.Users.AddRange(users);
-            int count = users.Length;
+            int count = users.Count;
             context.SaveChanges();
 
             return $"Successfully imported {count}";
