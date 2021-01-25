5f2083cf213342e25b4189f8ded2e4b32b99a4fb
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-25 12:44:30 +0200

    Add files to EF Core

---
+++ Entity Framework/JSON PROCESSING/JSON Demo Practice/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+using System.Text.Json.Serialization;
+namespace JSON_Demo_Practice
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            WeatherForecast weatherForecast = new WeatherForecast();
+            string weatherInfo = JsonSerializer.Serialize(weatherForecast);
+            Console.WriteLine(JsonSerializer.Serialize(weatherForecast, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true,
+                IgnoreNullValues = true
+            }));
+
+            File.WriteAllText("weather.json", weatherInfo);
+
+            string jsonString = File.ReadAllText("weather.json");
+            var weather = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+
+        }
+    }
+
+    class WeatherForecast
+    {
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public int TemperatureC { get; set; } = 30;
+        public string Summary { get; set; } = null;
+    }
+}
