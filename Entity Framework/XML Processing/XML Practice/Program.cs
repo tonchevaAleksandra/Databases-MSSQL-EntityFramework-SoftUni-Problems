3c59b69f3009032711004a51327c56e32761e962
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-28 12:08:56 +0200

    Add files

--- Entity Framework/XML Processing/XML Practice/Program.cs
+++ Entity Framework/XML Processing/XML Practice/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace XML_Practice
 {
@@ -6,7 +10,39 @@ namespace XML_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var xml = File.ReadAllText("Planes.xml");
+            ////XDocument xmlDocument = XDocument.Parse(xml);
+            //XDocument xmlDocument = XDocument.Load("Planes.xml");
+
+            //Console.WriteLine(xmlDocument.Declaration.Encoding);
+            //Console.WriteLine(xmlDocument.Root.Elements().Count());
+            //Console.WriteLine(xmlDocument.Root.Elements()
+            //    .FirstOrDefault(x=>x.Element("color").Value=="Blue").Element("year").Value);
+
+
+            XDocument xdocument = XDocument.Load("bgwiki-20210120-abstract.xml");
+            var articles = xdocument.Root.Elements()
+                .Where(x => x.Element("title").Value.Contains("Николай"))
+                .OrderBy(x => x.Element("title").Value)
+                .Select(x => new
+                {
+                    Title = x.Element("title").Value,
+                    Description = x.Element("abstract").Value,
+                    Url = x.Element("url").Value
+                });
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.Title);
+            }
         }
+    }
+
+    class Plane
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
     }
 }
