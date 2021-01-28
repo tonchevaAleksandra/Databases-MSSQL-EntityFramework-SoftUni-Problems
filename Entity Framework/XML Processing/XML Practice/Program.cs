1e47c36ddbe2c2982536a2079a4e0e6b26ab0696
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-28 12:03:34 +0200

    Add files to EF

---
+++ Entity Framework/XML Processing/XML Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
