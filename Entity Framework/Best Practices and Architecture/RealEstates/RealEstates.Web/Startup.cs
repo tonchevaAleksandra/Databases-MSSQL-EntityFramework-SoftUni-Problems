00e1f2f599053cad1b993c69b6747bac099afea2
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-01 19:47:47 +0200

    Add Configuration for ConnectionString

--- Entity Framework/Best Practices and Architecture/RealEstates/RealEstates.Web/Startup.cs
+++ Entity Framework/Best Practices and Architecture/RealEstates/RealEstates.Web/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Services;
 
 namespace RealEstates.Web
 {
@@ -23,12 +26,21 @@ namespace RealEstates.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RealEstateDbContext>(options =>
+            {
+                options.UseSqlServer(this.Configuration.GetConnectionString("RealEstate"));
+            });
+
+            services.AddTransient<IDistrictService, DistrictService>();
+            services.AddTransient<IPropertiesService, PropertiesService>();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            // Middlewares
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
