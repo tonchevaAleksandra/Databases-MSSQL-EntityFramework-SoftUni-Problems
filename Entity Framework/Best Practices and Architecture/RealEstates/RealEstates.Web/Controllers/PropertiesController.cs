0433a63284604f3477073f82c55c515509fe96b1
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-02 00:05:44 +0200

    Add files to RealEstates.Web/Controllers/PropertiesController.cs

---
+++ Entity Framework/Best Practices and Architecture/RealEstates/RealEstates.Web/Controllers/PropertiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RealEstates.Services;
+
+namespace RealEstates.Web.Controllers
+{
+    public class PropertiesController:Controller
+    {
+        private readonly IPropertiesService propertiesService;
+
+        public PropertiesController(IPropertiesService propertiesService)
+        {
+            this.propertiesService = propertiesService;
+            this.propertiesService = propertiesService;
+        }
+
+        public IActionResult Search()
+        {
+            return this.View();
+        }
+
+        public IActionResult DoSearch(int minPrice, int maxPrice)
+        {
+           var properties= this.propertiesService.SearchByPrice(minPrice, maxPrice);
+            return this.View(properties);
+        }
+    }
+}
