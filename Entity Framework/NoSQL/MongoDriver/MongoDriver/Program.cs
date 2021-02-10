c8e618a14394b7c629c826d87de7638f8b73741c
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-10 17:31:23 +0200

    Add MongoDriver/MongoDriver/MongoDriver.csproj

---
+++ Entity Framework/NoSQL/MongoDriver/MongoDriver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.WireProtocol;
+
+namespace MongoDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = client.GetDatabase("Softuni");
+            var collection = database.GetCollection<BsonDocument>("Students");
+
+            //SeedStudents(collection);
+
+            //PrintAllStudents(collection);
+
+
+        }
+
+        private static void PrintAllStudents(IMongoCollection<BsonDocument> collection)
+        {
+            var allStudents = collection.Find<BsonDocument>(new BsonDocument()).ToList();
+
+            foreach (var student in allStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        private static void SeedStudents(IMongoCollection<BsonDocument> collection)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var student = new BsonDocument
+                {
+                    {"name", "Pesho" + i}
+                };
+                collection.InsertOne(student);
+            }
+        }
+    }
+
+    class Student
+    {
+
+    }
+}
