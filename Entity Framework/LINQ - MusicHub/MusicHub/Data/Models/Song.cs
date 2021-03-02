0645587e309cfe71de13d0105a958ac1a12fb89c
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-03 00:32:40 +0200

    Add files to Entity Framework/LINQ - MusicHub

---
+++ Entity Framework/LINQ - MusicHub/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,30 @@
+﻿using MusicHub.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Genre Genre { get; set; }
+        [ForeignKey(nameof(Album))]
+        public int? AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+        public decimal Price { get; set; }
+
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public virtual ICollection<SongPerformer> SongPerformers => new HashSet<SongPerformer>();
+    }
+}
